d250fb407e9ddd7f0fd5b5c1cfefbbebfa512152
amrkhaled04 <105929016+amrkhaled04@users.noreply.github.com>
2023-03-22 12:11:09 +0200

    Update TrapCell.java

--- the_last_of_us_legacy/src/model/world/TrapCell.java
+++ the_last_of_us_legacy/src/model/world/TrapCell.java
@@ -1,7 +1,12 @@
 package model.world;
 
+import java.util.Random;
+
 public class TrapCell extends Cell {
-	private int trapDamage = 20;
+	private Random rand = new Random();
+	private int random = 10 * rand.nextInt(4);
+	
+	private int trapDamage = random;
 
 	public TrapCell() {
 		super();
