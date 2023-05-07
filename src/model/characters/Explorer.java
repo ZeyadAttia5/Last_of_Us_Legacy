356f54b443aa7dc64b891e617658817e8e08365e
Zeyad Attia <85710632+ZeyadAttia5@users.noreply.github.com>
2023-05-07 22:02:38 +0300

    added Explorer useSpecial

--- src/model/characters/Explorer.java
+++ src/model/characters/Explorer.java
@@ -1,5 +1,7 @@
 package model.characters;
 
+import model.world.*;
+import engine.Game;
 import exceptions.NoAvailableResourcesException;
 
 public class Explorer extends Hero {
@@ -14,6 +16,18 @@ public class Explorer extends Hero {
 	public void useSpecial() throws NoAvailableResourcesException {
 		//Whenever a supply is used, allows the player to see the entirety of the map
 		//for 1 turn
+		
+		if(this.getSupplyInventory().size() > 0) {
+			this.getSupplyInventory().get(0).use(this);
+			for (int row = 0; row < Game.map.length; row++) {
+				for(int col = 0; col < Game.map[row].length;col++) {
+					Game.map[row][col].setVisible(true);
+				}
+			}
+		}
+		else {
+			throw new exceptions.NoAvailableResourcesException("Insufficient Supply");
+		}
 	}
 
 	
