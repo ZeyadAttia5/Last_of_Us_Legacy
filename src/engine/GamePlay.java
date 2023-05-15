18b4f84b726f4ce5ffaf15605e15f06046866140
Zeyad Mohamed Attia <85710632+ZeyadAttia5@users.noreply.github.com>
2023-05-15 22:49:36 +0300

    Added a draft logo and message

--- src/engine/GamePlay.java
+++ src/engine/GamePlay.java
@@ -6,6 +6,9 @@ import javafx.scene.image.*;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.*;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 
 public class GamePlay extends Application {
@@ -19,9 +22,48 @@ public class GamePlay extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		primaryStageInit(primaryStage);
 		Group root = new Group();
-		Scene scene1 = new Scene(root, Color.BLANCHEDALMOND);
+		Scene scene1 = new Scene(root, Color.DARKORCHID);
+		Text text = new Text();
+		text.setText("Welcome to the Last of Us - Legacy");
+		text.setFont(Font.font("Arial",20));
+		text.setX(630);
+		text.setY(350);
+		text.setFill(Color.ANTIQUEWHITE);
 		
+		Line line = new Line();
+		line.setStartX(100);
+		line.setEndX(1000);
+		line.setStartY(100);
+		line.setEndY(600);
+		line.setStrokeWidth(5);
+		line.setStroke(Color.DARKORANGE);
 		
+		Rectangle rect = new Rectangle();
+		rect.setX(200);
+		rect.setY(200);
+		rect.setWidth(100);
+		rect.setHeight(300);
+//		rect.setStrokeWidth(5);
+//		rect.setStroke(Color.DARKSLATEBLUE);
+		
+		Circle circ = new Circle();
+		circ.setRadius(10);
+		circ.setCenterX(200);
+		circ.setCenterY(200);
+		circ.setFill(Color.BLUE);
+		
+		Image image = new Image("icons/logo.png");
+		ImageView imageView = new ImageView(image);
+		imageView.setX(390);
+		imageView.setY(-300);
+		imageView.setScaleX(0.3);
+		imageView.setScaleY(0.3);
+		imageView.setScaleZ(0.3);
+		
+		root.getChildren().add(imageView);
+		root.getChildren().add(text);
+//		root.getChildren().add(rect);
+//		root.getChildren().add(line);
 		primaryStage.setScene(scene1);
 		primaryStage.show();
 
